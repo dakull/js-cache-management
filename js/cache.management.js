3974dbe355ea39d99d86368546b3fa8beb0e80c8
Marian Posaceanu <marian@seesmic.com>
2011-10-26 12:58:49 +0300

    removed sha dep | cleaned up code a bit

--- js/cache.management.js
+++ js/cache.management.js
@@ -1,138 +1,119 @@
 /*
-*	Cache management classes
-*		author: Marian Posaceanu
-*		version: 1.0
-*		date: 11/04/2010
+*  Cache management classes
+*    author: Marian Posaceanu
+*    version: 1.1
+*    date: --/--/----
 */
 
-/* 
-*	clasa: ObjectKey 
-*	dependente: sha-1-v-1-0.js
+/*
+*  class: ObjectKey
+*  deps: sha-1-v-1-0.js
 */
-
-// constructor
-var ObjectKey = function(id_original) {
-	
-	this.id_original = id_original;
-	this.id_hash = sha1Hash(this.id_original);
-
+var ObjectKey = function(id_original,id_hash = '') {
+  
+  this.id_original = id_original;
+  if( id_hash == '' ) {
+    this.id_hash = sha1Hash(this.id_original);
+  } else {
+    this.id_hash = id_hash;
+  }
 };
 
-	// metode
-	ObjectKey.prototype = {
-		
-		getIdOriginal: function() {
-			return this.id_original;
-		},
-		getIdHash: function() {
-			return this.id_hash;
-		}
-		
-	};
+  ObjectKey.prototype = {
+    
+    getIdOriginal: function() {
+      return this.id_original;
+    },
+    getIdHash: function() {
+      return this.id_hash;
+    }
+    
+  };
 
-// end clasa ObjectKey
-
-
-/* 
-*	clasa: ObjectCreator 
-*	dependente: -
+/*
+*  class: ObjectCreator
+*  deps: - 
 */
-
-// constructor
 var ObjectCreator = function() {
-	this.raspuns = 'loading';
+  this.raspuns = 'loading';
 };
 
-	// metode
-	ObjectCreator.prototype = {
-		
-		getRaspuns: function() {	
-			return this.raspuns;
-		},
-		createObject: function(objKey,cache) {
-			var make_req_to = objKey.getIdOriginal();
-			
-			$.ajax({url: make_req_to,
-				 	context: this, 
-				 	success: function(html){
-				 		$('#project_dialog').append(html);
-	        			cache.addObject(objKey,html);
-					}
-	  		});
-		}
-	};
-// end clasa ObjectCreator
-
+  ObjectCreator.prototype = {
+    
+    getRaspuns: function() {  
+      return this.raspuns;
+    },
+    createObject: function(objKey,cache) {
+      var make_req_to = objKey.getIdOriginal();
+      
+      $.ajax({url: make_req_to,
+           context: this, 
+           success: function(html){
+             $('#project_dialog').append(html);
+                cache.addObject(objKey,html);
+          }
+        });
+    }
+  };
 
 /* 
-*	clasa: Cache
-*	dependente: -
+*  class: Cache
+*  deps: -
+*  example:
+*    this.cache['c27e3220e7ddbc7ab334099753efcf743d412624'] = "This is a test";
 */
-
-// constructor
 var Cache = function() {
-	console.log("Init Cache");
-	this.cache = new Array();
-	//this.cache['c27e3220e7ddbc7ab334099753efcf743d412624'] = "Acesta este un test";
-
+  console.log("Init Cache");
+  this.cache = new Array();
 };
 
-	// metode
-	Cache.prototype = {
-		
-		fetchObject: function(objKey) {
-			
-			for (var i in this.cache) {
-				if( i == objKey.getIdHash() ) {
-					return this.cache[i];
-				}
-			}
-			
-			return false;
-		
-		},
-		addObject: function(objKey,fresh_obj) {
-			console.log( "	Add to Cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
-			this.cache[ objKey.getIdHash() ] = fresh_obj;
-		}
-		
-	};
-
+  Cache.prototype = {
+    
+    fetchObject: function(objKey) {
+      
+      for (var i in this.cache) {
+        if( i == objKey.getIdHash() ) {
+          return this.cache[i];
+        }
+      }
+      
+      return false;
+    
+    },
+    addObject: function(objKey,fresh_obj) {
+      console.log( "  Add to Cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
+      this.cache[ objKey.getIdHash() ] = fresh_obj;
+    }
+    
+  };
 // end clasa CacheManager
-
 
 /* 
-*	clasa: CacheManager 
-*	dependente: -
+*  class: CacheManager 
+*  deps: -
+*  example:
+*   this.cache.addObject(objKey,fresh_obj);
 */
-
-// constructor
 var CacheManager = function() {
-	console.log("Init CacheManager");
-	this.objectCreator = new ObjectCreator();
-	this.cache = new Cache();
-
+  this.objectCreator = new ObjectCreator();
+  this.cache = new Cache();
 };
 
-	// metode
-	CacheManager.prototype = {
-		
-		fetchObject: function(objKey) {
-			
-			var buff = this.cache.fetchObject(objKey);
-			
-			if( buff === false ) {
-				console.warn( "Nu din cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
-				var fresh_obj = this.objectCreator.createObject(objKey,this.cache);
-				//this.cache.addObject(objKey,fresh_obj);
-				return fresh_obj;
-			} else {
-				console.info( "Obiectul preluat din cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
-				return buff;
-			}
-			
-		}
-		
-	};
-
-// end clasa CacheManager
+  CacheManager.prototype = {
+    
+    fetchObject: function(objKey) {
+      
+      var buff = this.cache.fetchObject(objKey);
+      
+      if( buff === false ) {
+        // console.warn( "Nu din cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
+        var fresh_obj = this.objectCreator.createObject(objKey,this.cache);
+        return fresh_obj;
+      } else {
+        // console.info( "Obiectul preluat din cache: ["+objKey.getIdOriginal()+"] "+objKey.getIdHash() );
+        return buff;
+      }
+      
+    }
+    
+  };
